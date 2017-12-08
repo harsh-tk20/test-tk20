59460aae36a362f507c4d8ca0a39e622f07f22d2
Harshdeep Singh <hSingh@TSTK.local>
2017-12-08 16:33:27 +0530

    first commit

---
+++ src/abc/Sample1.java
@@ -0,0 +1,16 @@
+package abc;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.gecko.driver", "E:\\selenium\\selenium files\\Drivers\\geckodriver\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.google.com");
+		driver.close();
+	}
+
+}
